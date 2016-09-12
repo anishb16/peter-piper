e2a70d85ada80f8636ec4355c2fa7c1013f6fd3a
anishb <bajagain.ansh@gmail.com>
2016-09-12 02:28:06 +0000

    Added for loop as well as counted P's in Peter Piper with function countOccurances

--- peter-piper.js
+++ peter-piper.js
@@ -22,4 +22,21 @@ var counter = 0;
 // TODO: Count the number of times an uppercase P or lowercase p character
 //       occurs in the `peterpiper` string.
 
+// Using for loop to count the number of times uppercase P or lowercase P 
+//for (var i = 0; i < peterpiper.length; i++) {
+ //   if (peterpiper.charAt(i) == 'P' || peterpiper.charAt(i) == 'p') {
+ //       counter++;
+//    }
+//}
+
+// For extra credit using the function countOccurances
+
+function countOccurances(string, char) { 
+   string = peterpiper.toUpperCase();
+   var occurance = string.split(char);
+     return occurance.length -1
+}
+
+counter = countOccurances(peterpiper, 'P');
+
 console.log("There are " + counter + " P's in Peter Piper.");
